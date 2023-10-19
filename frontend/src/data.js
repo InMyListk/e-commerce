0a5b6fa7eca2e1da9f124c54577b022f61fcc8c4
InMyListk <InMyListk@gmail.com>
2023-10-18 18:45:23 -0700

    Video-05-Create-Repository

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      catagory: 'Shirts',
+      image: '/images/p1.jpg', // 679px * 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high qulaity shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fat-shirt',
+      catagory: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high qulaity product',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      catagory: 'Pants',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high qulaity product',
+    },
+    {
+      name: 'Adidas Fit Pants',
+      slug: 'adidas-fat-pants',
+      catagory: 'Pants',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high qulaity product',
+    },
+  ],
+};
+
+export default data;

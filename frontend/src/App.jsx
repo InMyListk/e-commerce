64107073fa95cbe620f801110a23e8675e3325b1
InMyListk <InMyListk@gmail.com>
2023-10-18 17:03:48 -0700

    Video-03-React-App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      <div>
+        <header>
+          <a href="/">amazona</a>
+        </header>
+        <main>list products</main>
+      </div>
+    </>
+  );
+}

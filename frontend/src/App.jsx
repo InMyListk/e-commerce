ad00b531b1c757738bfd8fa12e7c62af582a24f1
InMyListk <InMyListk@gmail.com>
2023-10-20 15:25:35 -0700

    video-011-setup-bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,34 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import { Link } from 'react-router-dom';
+import { Navbar, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <div>
+        <div className="d-flex flex-column site-container">
           <header>
-            <Link to="/">amazona</Link>
+            <Navbar bg="dark" variant="dark">
+              <Container>
+                <LinkContainer to="/">
+                  <Navbar.Brand>amazona</Navbar.Brand>
+                </LinkContainer>
+              </Container>
+            </Navbar>
           </header>
           <main>
-            <Routes>
-              <Route path="/product/:slug" element={<ProductScreen />} />
-              <Route path="/" element={<HomeScreen />} />
-            </Routes>
+            <Container>
+              <Routes>
+                <Route path="/product/:slug" element={<ProductScreen />} />
+                <Route path="/" element={<HomeScreen />} />
+              </Routes>
+            </Container>
           </main>
+          <footer className="text-center">
+            <div>All rights reserved</div>
+          </footer>
         </div>
       </BrowserRouter>
     </>
